b5733a9b41198e94e8fa36a08917a2192c91bc96
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-04-15 18:49:21 +0800

    install material-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,13 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { Button } from "@material-ui/core"
+
 const IndexPage: React.FC<{}> = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <Button color="primary">Hello</Button>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
