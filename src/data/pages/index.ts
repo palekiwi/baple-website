094e66e8518c90532f0d7016e25505ab7a8e2318
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-04-17 18:46:57 +0800

    create LandingTemplate

--- src/data/pages/index.ts
+++ src/data/pages/index.ts
@@ -1,29 +1,29 @@
 export const frontmatter = {
-  template: "IndexTemplate",
+  template: "LandingTemplate",
   publish: false,
   sections: {
     categories: {
       heading: "Bienvenido a Baple Group",
-      image: "../img/background/baple-group-background.jpeg",
+      image: "../images/background/baple-group-background.jpeg",
       categoryLinks: [
         {
           label: "Baple Plastics",
-          image: "../img/logos/baple-plastics-logo.png",
+          image: "../../images/logos/baple-plastics-logo.png",
           to: "/plastics",
         },
         {
           label: "Baple Metal Packaging",
-          image: "../img/logos/baple-metal-packaging-logo.png",
+          image: "../../images/logos/baple-plastics-logo.png",
           to: "/metal-packaging",
         },
         {
           label: "Baple Energy",
-          image: "../img/logos/baple-energy-logo.png",
+          image: "../../images/logos/baple-plastics-logo.png",
           to: "/energy",
         },
         {
           label: "Baple Engineering",
-          image: "../img/logos/baple-engineering-logo.png",
+          image: "../../images/logos/baple-plastics-logo.png",
           to: "/engineering",
         },
       ],
---
+++ src/templates/LandingTemplate.tsx
@@ -0,0 +1,79 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+
+import { FluidObject } from "gatsby-image"
+
+interface CategoryLink {
+  label: string
+  to: string
+  image: {
+    childImageSharp: { fluid: FluidObject }
+  }
+}
+
+interface Props {
+  data: {
+    javascriptFrontmatter: {
+      fields: {
+        domain: string
+      }
+      frontmatter: {
+        sections: { categories: { categoryLinks: CategoryLink[] } }
+      }
+    }
+  }
+}
+
+const LandingTemplate: React.SFC<Props> = ({ data }) => {
+  console.log(
+    data.javascriptFrontmatter.frontmatter.sections.categories.categoryLinks
+  )
+  return (
+    <Layout>
+      <div>
+        <div>Index</div>
+        <div>
+          {data.javascriptFrontmatter.frontmatter.sections.categories.categoryLinks.map(
+            x => (
+              <div key={x.label}>
+                <div>
+                  <img src={x.image.childImageSharp.fluid.src} />
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default LandingTemplate
+
+export const query = graphql`
+  query($slug: String!) {
+    javascriptFrontmatter(fields: { slug: { eq: $slug } }) {
+      fields {
+        domain
+      }
+      frontmatter {
+        sections {
+          categories {
+            categoryLinks {
+              label
+              to
+              image {
+                childImageSharp {
+                  fluid(quality: 100, maxWidth: 400) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
