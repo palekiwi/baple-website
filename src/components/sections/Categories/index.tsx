63d06ea7efd3741c9c9a3223bb3168fadf2a7537
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-04-19 22:54:13 +0800

    fix categories

--- src/components/sections/Categories/index.tsx
+++ src/components/sections/Categories/index.tsx
@@ -18,7 +18,7 @@ const Categories: React.FC<Props> = props => {
   return (
     <Grid container spacing={3} className={classes.cards}>
       {props.categoryLinks.map(x => (
-        <Grid key={x.to} item xs={12} sm={6} md={3}>
+        <Grid key={x.to} item xs={12} sm={6} md={4} lg={3}>
           <Link to={x.to}>
             <Card className={classes.card}>
               <CardActionArea className={classes.cardAction}>
