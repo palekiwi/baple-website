4339e586db36446b1b5b6b85e7639b2479641e04
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-04-26 21:08:51 +0800

    add SVG Logos

--- src/components/DivisionsList/index.tsx
+++ src/components/DivisionsList/index.tsx
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/core"
 import { Link } from "gatsby"
 import { AppData } from "../../types"
+import Logo from "../../components/LogoSVG"
 
 import useStyles from "./styles"
 
@@ -27,10 +28,7 @@ const DivisionsList: React.FC<Props> = ({ divisions }) => {
               <Card className={classes.card}>
                 <CardActionArea className={classes.cardAction}>
                   <CardContent className={classes.content}>
-                    <img
-                      className={classes.image}
-                      src={division.logo.text.childImageSharp.fluid.src}
-                    />
+                    <Logo fill="red" domain={division.name} />
                     <Typography color="inherit" className={classes.label}>
                       {division.title}
                     </Typography>
