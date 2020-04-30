df291abea6a0b44efd06c14a994f3e091a43460a
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-04-30 08:28:20 +0800

    add coronavirus/promo

---
+++ src/data/products/coronavirus/promo.ts
@@ -0,0 +1,33 @@
+export const frontmatter = {
+  title: "Protective Equipment",
+  sections: {
+    gridList: {
+      items: [
+        {
+          heading: "Disposable Gloves",
+          image: "../../../images/coronavirus/gloves.jpeg",
+        },
+        {
+          heading: "Face Mask",
+          image: "../../../images/coronavirus/face-mask-001.jpeg",
+        },
+        {
+          heading: "Isolation Gown",
+          image: "../../../images/coronavirus/isolation-gown.jpeg",
+        },
+        {
+          heading: "KN95",
+          image: "../../../images/coronavirus/n95.jpeg",
+        },
+        {
+          heading: "Face Protection",
+          image: "../../../images/coronavirus/face-shield.jpeg",
+        },
+        {
+          heading: "Eye Protection",
+          image: "../../../images/coronavirus/eye-protection.jpeg",
+        },
+      ],
+    },
+  },
+}
