388191c22ea9d41af45672f4ecc33e676187b505
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-05-07 10:08:37 +0800

    update email

--- src/data/siteMetadata/index.js
+++ src/data/siteMetadata/index.js
@@ -1,16 +1,10 @@
-//const group = require("./group")
-//const metalPackaging = require("./metalPackaging")
-//const plastics = require("./plastics")
-//const energy = require("./energy")
-//const engineering = require("./engineering")
-
 module.exports = {
   title: "Baple",
   author: "Pawel Lisewski",
   description: "Website of Baple Group",
   contact: {
     name: "HQ",
-    email: "info@baple.com.tw",
+    email: "info@baple.es",
     phone: "+886-2-2897-0076",
     address: ["4F, No. 384, Daye Rd", "Beitou District, Taipei", "Taiwan"],
     socialMedia: {
@@ -19,6 +13,4 @@ module.exports = {
       twitter: "",
     },
   },
-  //domains: [group, energy, engineering, plastics, metalPackaging],
-  //domains: [group],
 }
